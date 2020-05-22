79ac94df8306e562d3efc6a7a66dcbec46c3ed3d
viscaino <viscaino@users.noreply.github.com>
2020-05-22 02:19:18 -0300

    20200522_0219

--- storage/block_volume.tf
+++ storage/block_volume.tf
@@ -1,13 +1,25 @@
 # Terraform v0.12 is assumed
 // Created by Bruno Viscaino
 
-data "oci_core_instances" "data_instances" {
-    compartment_id  = "${lookup(data.oci_identity_compartments.my_compute_comp.compartments[0], "id")}"
+
+data "oci_identity_compartments" "my_storage_comp" {
+    depends_on      = ["oci_identity_compartment.child_compartment"]
+    compartment_id  = "${oci_identity_compartment.parent_compartment.id}"
+
+    filter {
+        name    = "name"
+        values  = ["${var.env_prefix}\\w*Storage"]
+        regex   = true
+    }
 }
 
-output "data_instance_ouput_ocid" {
+output "my_storage_comp_output" {
     depends_on  = ["oci_core_instances.data_instances"]
-    value       = "${lookup(data.oci_identity_compartments.my_compute_comp.compartments[0], "id")}"
+    value       = "${data.oci_identity_compartments.my_storage_comp.compartments}"
+}
+
+data "oci_core_instances" "data_instances" {
+    compartment_id  = "${lookup(data.oci_identity_compartments.my_compute_comp.compartments[0], "id")}"
 }
 
 output "data_instances_output" {
@@ -19,7 +31,7 @@ resource "oci_core_volume" "create_volume" {
     for_each            = "${var.server_list}"
     depends_on          = ["oci_core_instance.my_pub_instance"]
     availability_domain = "${each.value["ad"]}"
-    compartment_id      = "${lookup(data.oci_identity_compartments.my_compute_comp.compartments[0], "id")}"
+    compartment_id      = "${lookup(data.oci_identity_compartments.my_storage_comp.compartments[0], "id")}"
     display_name        = "${each.value["volname"]}"
     size_in_gbs         = "${each.value["volsize"]}"
 }
